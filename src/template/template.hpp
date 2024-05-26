b9e9fef5a45c2f6863fe70952a9676925546bfbe
りすりす/TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2024-05-26 19:18:48 +0900

    Update template.hpp for TwoSquirrels

--- src/template/template.hpp
+++ src/template/template.hpp
@@ -1,22 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-const ll INF = LLONG_MAX / 4;
-#define rep(i, a, b) for(ll i = a; i < (b); i++)
+const ll INF = 4e18;
+using ld = long double;
+const ld INFD = INFINITY;
+const ld EPS = LDBL_EPSILON;
+template <class T, class Comp = less<T>> using p_que = priority_queue<T, vector<T>, Comp>;
+array sin90 = { 0, 1, 0, -1 }, cos90 = { 1, 0, -1, 0 };
+#define p1 first
+#define p2 second
+#define reps(i, l, r) for (ll i = (l), i##_right = (r); i < i##_right; i++)
+#define rep(i, n) reps(i, 0, n)
 #define all(a) begin(a), end(a)
 #define sz(a) ssize(a)
-bool chmin(auto& a, auto b) {
-   if(a <= b) return 0;
-   a = b;
-   return 1;
-}
-bool chmax(auto& a, auto b) {
-   if(a >= b) return 0;
-   a = b;
-   return 1;
-}
+bool chmin(auto &&a, auto b) { return a > b && (a = b, 1); }
+bool chmax(auto &&a, auto b) { return a < b && (a = b, 1); }
 
 int main() {
    cin.tie(0)->sync_with_stdio(0);
+   cout << fixed << setprecision(20);
+
    // your code here...
 }
