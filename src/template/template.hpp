62ae4e458c77679c877907ceae7d103c70e966a2
ZOI-dayo <zoizoi.dayo@gmail.com>
2024-06-29 10:04:46 +0900

    dumpマクロの追加

--- src/template/template.hpp
+++ src/template/template.hpp
@@ -16,6 +16,34 @@ array sin90 = {0, 1, 0, -1}, cos90 = {1, 0, -1, 0};
 bool chmin(auto& a, auto b) { return a > b ? a = b, 1 : 0; }
 bool chmax(auto& a, auto b) { return a < b ? a = b, 1 : 0; }
 
+#define DEBUG
+
+#define DUMPOUT cerr
+void dump_func() {
+    DUMPOUT << endl;
+}
+template <class Head, class... Tail>
+void dump_func(Head &&head, Tail &&... tail) {
+    DUMPOUT << head;
+    if (sizeof...(Tail) > 0) {
+        DUMPOUT << ", ";
+    }
+    dump_func(std::move(tail)...);
+}
+#ifdef DEBUG
+#define DEB
+#define dump(...)                                                              \
+    DUMPOUT << "\033[1m\033[41m"                                               \
+            << "  " << string(#__VA_ARGS__) << ": "                            \
+            << "[" << to_string(__LINE__) << ":" << __FUNCTION__ << "]"        \
+            << endl                                                            \
+            << "\033[49m\033[0m    ",                                                         \
+        dump_func(__VA_ARGS__)
+#else
+#define DEB if (false)
+#define dump(...)
+#endif
+
 int main() {
    cin.tie(0)->sync_with_stdio(0);
    cout << fixed << setprecision(20);
